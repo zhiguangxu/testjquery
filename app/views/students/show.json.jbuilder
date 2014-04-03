ce9d3cc57ea342da1339d4360905ed2b7598e95b
Zhiguang Xu <xuzhiguang@gmail.com>
2014-04-03 16:36:29 -0400

    sample code

---
+++ app/views/students/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @student, :id, :name, :address, :created_at, :updated_at
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestjQuery::Application.config.session_store :cookie_store, key: '_testjQuery_session'
